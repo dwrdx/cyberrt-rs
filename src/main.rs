c27bc72af8d949edf917cf52980fd82e5cfa7c79
Edward XU <edward.xu@hotmail.com>
2024-10-31 21:36:22 +0800

    support periodic exectuion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use libloading::{Library, Symbol};
 use std::path::Path;
+use tokio::time::{self, Duration};
 
-fn main() {
+use std::fs;
+use json;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+
+    let json_content = fs::read_to_string("test.json")
+        .expect("Failed to read JSON file");
+
+    // 解析 JSON 内容
+    let parsed = json::parse(&json_content)?;
+
+    // 访问 JSON 中的字段
+    let name = parsed["name"].as_str().unwrap_or("Unknown");
+    let period_ms = parsed["period_ms"].as_u64().unwrap_or(0);
+
+
+
     // 动态加载.so文件
     let lib_path = Path::new("/Users/exu/Workarea/cyberrt-rs/tests/libmylib.so");
     
@@ -13,9 +31,6 @@ fn main() {
         // 定义 hello_from_c 函数的类型
         let hello_from_c: Symbol<unsafe extern "C" fn()> =
             lib.get(b"hello_from_c").expect("Failed to load function hello_from_c");
-
-        // 调用 hello_from_c 函数
-        hello_from_c();
 
         // 定义 add 函数的类型
         let add: Symbol<unsafe extern "C" fn(i32, i32) -> i32> =
@@ -29,6 +44,27 @@ fn main() {
             lib.get(b"proc").expect("Failed to load function proc");
 
         // 调用 hello_from_c 函数
+        tokio::spawn(periodic_task(*hello_from_c, period_ms)).await.unwrap();
+
         proc();
+
+
+    }
+    Ok(())
+}
+
+
+
+
+async fn periodic_task(proc: unsafe extern "C" fn(), period_ms: u64) {
+    let mut interval = time::interval(Duration::from_micros(period_ms)); // 每5秒触发一次
+
+    loop {
+        interval.tick().await;
+        // 这里放置要周期性执行的代码
+        unsafe {
+            proc();
+        }
+        println!("Periodic task is running in {}ms!", period_ms);
     }
 }
